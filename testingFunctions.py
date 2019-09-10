afaa84c51e9bce81578549b29e1b93c478e93193
Suzanne Robinson <ee14s2r@leeds.ac.uk>
2019-09-10 16:45:44 +0100

    Updated after day 2

---
+++ testingFunctions.py
@@ -0,0 +1,15 @@
+# Basic function for testing user restrictions
+
+
+def maths(x):
+    If x=0:
+        return ValueError:
+            print('The input to this function cannot be zero')
+    a= x*2
+    b= a**2
+    c = b-1
+    
+    return c
+
+
+test1= maths(0)
